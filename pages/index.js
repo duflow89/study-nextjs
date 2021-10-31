ee01b2a3f6683a51233f25dfc95039da7bcff7a7
duflow89 <duflow89@gmail.com>
2021-11-01 08:58:49 +0900

    study: https://nextjs.org/learn/basics/dynamic-routes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
 
@@ -25,11 +26,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
               {id}
               <br />
-              {date}
+              {date}+
             </li>
           ))}
         </ul>
