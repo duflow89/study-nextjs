3d4a5e760706d05ec9564652d708e0364968b68f
duflow89 <duflow89@gmail.com>
2021-11-11 13:21:25 +0900

    study: manual setup (wip)

---
+++ study-manual-setup/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Study Manual Setup</title>
+        <meta name="description" content="Study Manual Setup" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Study Manual Setup</h1>
+    </div>
+  );
+};
+
+export default Home;
