79e000059a02d266782f05ec58c095a2bcb429f2
cocktail-nick <hj.lee@cocktailfd.com>
2022-06-26 15:11:38 +0900

    chore: reset

--- tutorial/components/date.js
+++
@@ -1,8 +0,0 @@
-import { parseISO, format } from 'date-fns';
-
-const Date = ({ dateString }) => {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'yyyy MM dd')}</time>;
-};
-
-export default Date;
